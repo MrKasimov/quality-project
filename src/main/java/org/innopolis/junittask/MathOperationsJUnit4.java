49ff5dda7c927ff0a370d33628e886abeb261cf3
Nurdaulet Kunkhozhaev <n.kunkhozhaev@gmail.com>
2022-11-28 16:49:55 +0300

    Junit4 and Junit5 tests are done

---
+++ src/main/java/org/innopolis/junittask/MathOperationsJUnit4.java
@@ -0,0 +1,12 @@
+package org.innopolis.junittask;
+
+public class MathOperationsJUnit4 {
+
+    public int addJ4(int a, int b) {
+        return a + b;
+    }
+
+    public int substractJ4(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/main/java/org/innopolis/junittask/MathOperationsJUnit5.java
@@ -0,0 +1,12 @@
+package org.innopolis.junittask;
+
+public class MathOperationsJUnit5 {
+
+    public int addJ5(int a, int b) {
+        return a + b;
+    }
+
+    public int multiplyJ5(int a, int b) {
+        return a * b;
+    }
+}
