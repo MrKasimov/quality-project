b373e8b2167dc3fe12716db77f46a1069cb23e09
MrKasimov <oibekkasymov@gmail.com>
2022-11-28 18:04:20 +0300

    Add code without tests

--- src/main/java/org/innopolis/functions/Sum.java
+++ src/main/java/org/innopolis/functions/Sum.java
@@ -5,4 +5,8 @@ public class Sum {
     public static Integer sum(Integer a, Integer b) {
         return a + b;
     }
+
+    public static Integer minus(Integer a, Integer b) {
+        return a - b;
+    }
 }
